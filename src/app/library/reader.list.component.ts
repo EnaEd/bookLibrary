8e2e330c240d43ceeb17092e6973806feb7ecf49
developer <djaziz12393@gmail.com>
2019-11-29 10:43:44 +0200

    add bookInHand

--- src/app/library/reader.list.component.ts
+++ src/app/library/reader.list.component.ts
@@ -1,18 +1,28 @@
+import { Reader } from './../models/reader';
 import { ReaderRepository } from './../repositories/reader.repository';
+import { BookRepository } from './../repositories/book.repository';
 import { Component } from "@angular/core";
-import { Reader } from '../models/reader';
+import { Book } from '../models/book.model';
 
 @Component({
     selector: "readerList",
-    templateUrl:"reader.list.component.html"
+    templateUrl: "reader.list.component.html"
 })
-export class ReaderListComponent{
-    
-    constructor(private readerRepository:ReaderRepository) {
-        
+export class ReaderListComponent {
+
+    public selectedReader:Reader = null;
+
+    constructor(private readerRepository: ReaderRepository, private bookRepository: BookRepository) {
+
     }
 
-    get Readers():Reader[]{
+    get Readers(): Reader[] {
         return this.readerRepository.getReaders();
     }
+
+    get BooksInHand(): Book[] {
+        return this.readerRepository.getBooksInHand(this.selectedReader);
+    }
+
+    
 }
--- src/app/repositories/reader.repository.ts
+++ src/app/repositories/reader.repository.ts
@@ -1,17 +1,33 @@
 import { StaticDataSource } from './../models/static.data.source';
 import { Reader } from './../models/reader';
 import { Injectable } from "@angular/core";
+import { Book } from '../models/book.model';
 
 @Injectable()
 export class ReaderRepository {
-    private readers: Reader[]=[];
+    private readers: Reader[] = [];
+    private books: Book[] = [];
 
     constructor(private dataSource: StaticDataSource) {
         dataSource.getReaders().subscribe(data => {
             this.readers = data;
         });
+        dataSource.getBooks().subscribe(data => {
+            this.books = data;
+        });
     }
-    getReaders(){
+    getReaders() {
         return this.readers;
+    }
+
+    getBooksInHand(reader: Reader = null): Book[] {
+        return this.books.filter(x => {
+            if (reader == null) {
+                let bookIds = [].concat(...this.readers.map(item => item.books)).
+                    filter((value, index, self) => self.indexOf(value) == index);//filter for distinct
+                return bookIds.indexOf(x.id)>=0;
+            }
+            return reader.books.indexOf(x.id) >= 0;
+        });
     }
 }
